b018dc72f6f0ff7d32296c70018d5e52d0be99b2
PrimusAbOris <guy.ross.rit@gmail.com>
2023-09-18 17:18:24 -0400

    Finding what variables I'll need

--- src/com/coderscampus/assignment4/ArraySortService.java
+++ src/com/coderscampus/assignment4/ArraySortService.java
@@ -1,6 +1,14 @@
 package com.coderscampus.assignment4;
 
 public class ArraySortService {
+	Student[] compSciStudents; // this will become course1.csv
+	Student[] apmthStudents; // course2.csv
+	Student[] statStudents; // course3.csv
+	
+	
+	
+	
+	
 	public static String[] arraySort() {
 		String[] arr = {"test1", "test2", "test3"};
 		return arr;
--- src/com/coderscampus/assignment4/GR_Assignment4.java
+++ src/com/coderscampus/assignment4/GR_Assignment4.java
@@ -1,9 +1,26 @@
 package com.coderscampus.assignment4;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class GR_Assignment4 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws FileNotFoundException {
+		Student[] allStudents; //this will be populated first
+		Student[] compSciStudents; // this will become course1.csv
+		Student[] apmthStudents; // course2.csv
+		Student[] statStudents; // course3.csv
+		
+	
+		BufferedReader br = new BufferedReader(new FileReader("student-master-list.csv"));
+		
+		
+		String[] arrayFromCSV;
+		
+		ArraySortService.arraySort();
+		// FileWriteService.writeToFile;
+		// course1.csv, course2.csv, course3.csv
 
 	}
 

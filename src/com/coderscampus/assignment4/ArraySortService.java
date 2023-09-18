d4d733f373020b6b491a99cdfb7e2f6865284a0e
PrimusAbOris <guy.ross.rit@gmail.com>
2023-09-18 06:22:20 -0400

    Preliminary skeleton

---
+++ src/com/coderscampus/assignment4/ArraySortService.java
@@ -0,0 +1,8 @@
+package com.coderscampus.assignment4;
+
+public class ArraySortService {
+	public static String[] arraySort() {
+		String[] arr = {"test1", "test2", "test3"};
+		return arr;
+	}
+}
---
+++ src/com/coderscampus/assignment4/GR_Assignment4.java
@@ -0,0 +1,10 @@
+package com.coderscampus.assignment4;
+
+public class GR_Assignment4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

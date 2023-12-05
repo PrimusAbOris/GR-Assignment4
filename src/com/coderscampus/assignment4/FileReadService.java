b46936c3e893fdce2825e032c389ad8543812534
PrimusAbOris <guy.ross.rit@gmail.com>
2023-12-05 13:49:58 -0500

    Update FileReadService.java

--- src/com/coderscampus/assignment4/FileReadService.java
+++ src/com/coderscampus/assignment4/FileReadService.java
@@ -6,10 +6,9 @@ import java.io.IOException;
 
 public class FileReadService {
 	
-	public static Student[] getStudentArray(String filename) throws IOException {
+	public Student[] getStudentArray(String filename) throws IOException {
 		// takes in the filename, returns the array of all students in all subjects
 		BufferedReader br = new BufferedReader(new FileReader(filename));
-		String[] garbageLine = br.readLine().split(",");
 		
 		Student[] allStudents = new Student[GR_Assignment4.FILE_LENGTH + 1];
 		String[] line = null;
@@ -19,12 +18,13 @@ public class FileReadService {
 				line = br.readLine().split(",");
 			}
 			System.out.println(line[0] + line[1] + line[2] + line[3]); //test
-			int studentId = Integer.parseInt(line[0]);
+			String studentId = line[0];
 			String name = line[1];
 			String course = line[2];
-			int grade = Integer.parseInt(line[3]);
+			String grade = line[3];
 			Student s = new Student(studentId, name, course, grade);
 			if (s != null) allStudents[i] = s;
+			// i++
 		}
 		br.close();
 		return allStudents;
